7bb38050ec4b00bc6ebe685002a0784f6aa79f6f
SAKSHI BHATIA <145453542+Sakshibhatia06@users.noreply.github.com>
2025-11-03 18:49:58 +0530

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -171,12 +171,15 @@ const Header = () => {
 
         {/* Right Buttons */}
         <div className="hidden xl:block">
-          <Link
-            href="/signup"
-            className="hidden xl:block bg-[#B89236] text-white font-semibold px-6 py-2 rounded-md"
-          >
-            Sign Up
-          </Link>
+          <a
+  href="https://wa.me/91XXXXXXXXXX?text=Hi%20I%20need%20homework%20help"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="bg-gradient-to-r from-[#f8d88c] to-[#c8912b] text-[#6a480f] font-bold px-10 py-3 rounded-lg text-lg shadow-lg border border-[#eac16c] hover:opacity-90 transition block text-center"
+>
+  Get Homework Help now!
+</a>
+
         </div>
       </div>
     </header>
